7148c3b4f093da859be44db761fb7d06a7020a03
JaccovanWijk <jaccov.wijk@yahoo.com>
2019-05-09 16:49:08 +0200

    Werkende t/m 6.1

--- Groepsopdracht 1/matchlang.py
+++ Groepsopdracht 1/matchlang.py
@@ -1,1 +1,35 @@
 from langdetect import *
+import os
+
+class LangMatcher:
+
+    def __init__(self, path):
+
+        # create dict
+        self.frequencies = {}
+
+        # fill dict with key language and value a dict of frequencies
+        for filename in os.listdir(path):
+            language = filename[:filename.index(".")]
+            data = read_trigrams(path + "/" + filename)
+            self.frequencies[language] = data
+
+    def score(self, text, n=1, ngrams=200):
+
+        # ask for the ngram table
+        table = trigram_table(text, ngrams)
+
+        # calculate scores and make dict with languages and scores
+        scores = {}
+        for language in self.frequencies:
+            scores[language] = cosine_similarity(table, self.frequencies[language])
+
+        return dict(sorted(scores.items(), key=operator.itemgetter(1), reverse=True)[:n])
+
+    def recognize(self, filename, encoding="utf-8", ngrams=200):
+
+        with open(filename, encoding=encoding) as file:
+            return self.score(file.read(), ngrams=ngrams)
+
+app = LangMatcher("./trigram-models")
+print(app.recognize("./training/Bambara-UTF8"))
