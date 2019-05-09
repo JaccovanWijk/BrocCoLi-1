6bd41ea046454e25482d2e30525fc3740bfa94b2
Gabi van der Kooij <g.l.vanderkooij@students.uu.nl>
2019-05-09 15:43:47 +0200

    Hiero

--- Groepsopdracht 1/write_profiles.py
+++ Groepsopdracht 1/write_profiles.py
@@ -1,24 +1,24 @@
 from langdetect import *
+import os
 
-if __name__ == "__main__" :
-    main()
-
-def __main__(self,args):
-    make_profiles()
 
 def make_profiles(datafolder="./training", profilefolder="./trigram-models", size = 200):
 
     # Create the folder
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+    if not os.path.exists(profilefolder):
+        os.makedirs(profilefolder)
 
     trainingFiles = os.listdir(datafolder) # List all the files in the folder
     for fileName in trainingFiles:
         name = fileName.split("-")
         language = name[0]
-        encoding = name[1]
-        with open(fileName, encoding=encoding) as file :
+        enc = name[1]
+        with open(fileName, encoding=enc) as file :
             table = trigram_table(fileName.read(), size)
             write_trigrams(table,language+"."+str(size))
 
+def __main__(self,args):
+    make_profiles()
 
+if __name__ == "__write_profiles__" :
+    main()
