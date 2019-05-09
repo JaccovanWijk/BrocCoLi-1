cffd491230a4e96c19f14edbf26288aafb6d0934
Gabi van der Kooij <g.l.vanderkooij@students.uu.nl>
2019-05-09 15:37:35 +0200

    write_profiles +1

--- Groepsopdracht 1/write_profiles.py
+++ Groepsopdracht 1/write_profiles.py
@@ -1,1 +1,24 @@
 from langdetect import *
+
+if __name__ == "__main__" :
+    main()
+
+def __main__(self,args):
+    make_profiles()
+
+def make_profiles(datafolder="./training", profilefolder="./trigram-models", size = 200):
+
+    # Create the folder
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    trainingFiles = os.listdir(datafolder) # List all the files in the folder
+    for fileName in trainingFiles:
+        name = fileName.split("-")
+        language = name[0]
+        encoding = name[1]
+        with open(fileName, encoding=encoding) as file :
+            table = trigram_table(fileName.read(), size)
+            write_trigrams(table,language+"."+str(size))
+
+
