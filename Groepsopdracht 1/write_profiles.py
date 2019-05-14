d560cd9aa1cb370d02a9678371c0703e405f842e
Gabi van der Kooij <g.l.vanderkooij@students.uu.nl>
2019-05-14 16:20:35 +0200

    Okee

--- Groepsopdracht 1/write_profiles.py
+++ Groepsopdracht 1/write_profiles.py
@@ -3,11 +3,13 @@ import os
 
 def make_profiles(datafolder="./training", profilefolder="./trigram-models", size = 200):
 
-    # Create the folder
+    # Create the folder, if it doens't exist
     if not os.path.exists(profilefolder):
         os.makedirs(profilefolder)
 
     trainingFiles = os.listdir(datafolder) # List all the files in the folder
+
+    # For all the files, write their trigrams to a new file
     for fileName in trainingFiles:
         name = fileName.split("-")
         language = name[0]
